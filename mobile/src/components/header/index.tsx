a6d4a2b6aacb86720674ceaba48a904ed42cbdcd
Ofoegbu Goodnews <goodnews@softcom.ng>
2021-10-20 23:43:47 +0100

    Configured react-native-vector-icons to work with ios and android

--- mobile/src/components/header/index.tsx
+++ mobile/src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View} from 'react-native'
 
+import Icon from 'react-native-vector-icons/FontAwesome5'
 import React from 'react'
 
 interface Props {}
@@ -8,6 +9,7 @@ const HeaderComponent: React.FC<Props> = () => {
   return (
     <View>
       <Text>App Header</Text>
+      <Icon name="apple-alt" size={30} color="#900" />
     </View>
   )
 }
