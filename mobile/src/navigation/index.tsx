3817a17694d3319ad88f7739c843de25d9d063f7
Ofoegbu Goodnews <goodnews@softcom.ng>
2021-10-24 10:25:30 +0100

    Started working on meeting room page

--- mobile/src/navigation/index.tsx
+++ mobile/src/navigation/index.tsx
@@ -9,13 +9,16 @@ const Navigation = () => {
   const Stack = createStackNavigator()
   const screenOptions = {
     headerShown: true,
+    gestureDirection: 'vertical'
   }
 
   return (
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName={routeKeys.home}
-        screenOptions={screenOptions}>
+        screenOptions={{
+          gestureDirection: 'vertical'
+        }}>
         <Stack.Screen
           name={routeKeys.home}
           component={HomeScreen}
